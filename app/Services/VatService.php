5d1292afdd27c676324b28adc31b913e0330712a
AlexandruCiontu <62359556+AlexandruCiontu@users.noreply.github.com>
2025-07-22 05:43:37 +0300

    replace abandoned VAT package

--- app/Services/VatService.php
+++ app/Services/VatService.php
@@ -2,10 +2,17 @@
 
 namespace App\Services;
 
-use Dannyvankooten\LaravelVat\Facades\Rates;
+use Ibericode\Vat\Rates;
 
 class VatService
 {
+    protected Rates $rates;
+
+    public function __construct()
+    {
+        $this->rates = new Rates(storage_path('app/vat-rates.cache'));
+    }
+
     public function calculate(float $netAmount, string $rateType, ?string $countryCode = null): array
     {
         $countryCode = $countryCode ?: session('country_code');
@@ -13,7 +20,12 @@ class VatService
             return ['rate' => 0, 'vat' => 0, 'gross' => $netAmount];
         }
 
-        $rate = Rates::country($countryCode, $rateType) ?? 0;
+        try {
+            $rate = $this->rates->getRateForCountry($countryCode, $rateType);
+        } catch (\Throwable $e) {
+            $rate = 0;
+        }
+
         $vat = round($netAmount * $rate / 100, 2);
         $gross = round($netAmount + $vat, 2);
 
