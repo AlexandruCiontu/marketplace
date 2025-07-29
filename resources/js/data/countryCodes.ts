46be3e3831aa5eca34366655f144374f12bad6d8
AlexandruCiontu <deliveriesml@gmail.com>
2025-07-29 07:57:16 +0100

    update countrycodes

--- resources/js/data/countryCodes.ts
+++ resources/js/data/countryCodes.ts
@@ -1,5 +1,111 @@
 export const countryCodes = [
-  { code: '+40', name: 'Romania' },
-  { code: '+359', name: 'Bulgaria' },
-  { code: '+36', name: 'Hungary' },
+  {
+    "code": "+43",
+    "name": "Austria"
+  },
+  {
+    "code": "+32",
+    "name": "Belgium"
+  },
+  {
+    "code": "+359",
+    "name": "Bulgaria"
+  },
+  {
+    "code": "+357",
+    "name": "Cyprus"
+  },
+  {
+    "code": "+420",
+    "name": "Czech Republic"
+  },
+  {
+    "code": "+45",
+    "name": "Denmark"
+  },
+  {
+    "code": "+49",
+    "name": "Germany"
+  },
+  {
+    "code": "+372",
+    "name": "Estonia"
+  },
+  {
+    "code": "+30",
+    "name": "Greece"
+  },
+  {
+    "code": "+34",
+    "name": "Spain"
+  },
+  {
+    "code": "+358",
+    "name": "Finland"
+  },
+  {
+    "code": "+33",
+    "name": "France"
+  },
+  {
+    "code": "+385",
+    "name": "Croatia"
+  },
+  {
+    "code": "+39",
+    "name": "Italy"
+  },
+  {
+    "code": "+371",
+    "name": "Latvia"
+  },
+  {
+    "code": "+370",
+    "name": "Lithuania"
+  },
+  {
+    "code": "+352",
+    "name": "Luxembourg"
+  },
+  {
+    "code": "+36",
+    "name": "Hungary"
+  },
+  {
+    "code": "+353",
+    "name": "Ireland"
+  },
+  {
+    "code": "+356",
+    "name": "Malta"
+  },
+  {
+    "code": "+31",
+    "name": "Netherlands"
+  },
+  {
+    "code": "+48",
+    "name": "Poland"
+  },
+  {
+    "code": "+351",
+    "name": "Portugal"
+  },
+  {
+    "code": "+40",
+    "name": "Romania"
+  },
+  {
+    "code": "+386",
+    "name": "Slovenia"
+  },
+  {
+    "code": "+421",
+    "name": "Slovakia"
+  },
+  {
+    "code": "+46",
+    "name": "Sweden"
+  }
 ];
+
