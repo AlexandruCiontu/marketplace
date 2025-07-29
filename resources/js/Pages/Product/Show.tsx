90ab5e8ad7b38218248104f32dab270b98c46a2f
AlexandruCiontu <62359556+AlexandruCiontu@users.noreply.github.com>
2025-07-30 00:02:04 +0300

    Log product details for debugging

--- resources/js/Pages/Product/Show.tsx
+++ resources/js/Pages/Product/Show.tsx
@@ -11,7 +11,7 @@ import { useVatCountry } from '@/hooks/useVatCountry';
 
 function Show(props: PageProps<{ product: Product; variationOptions: number[] }>) {
   const { appName, product, variationOptions } = props
-  console.log(props)
+  console.log(product)
   const form = useForm<{
     option_ids: Record<string, number>;
     quantity: number;
